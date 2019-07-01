3456f04f49e08d079c29bbde7081cb7df6b63933
Roy Hooper <rhooper@toybox.ca>
2019-07-01 16:19:32 -0400

    fix example lint

--- examples/pypixelbuf_simpletest.py
+++ examples/pypixelbuf_simpletest.py
@@ -3,14 +3,14 @@ import pypixelbuf
 callback_called = False
 
 
-def callback():
+def callback():  # pylint: disable=global-statement
     global callback_called
     callback_called = True
 
 
-buffer = pypixelbuf.PixelBuf(20, bytearray(20 * 3), pypixelbuf.RGB, 1.0, auto_write=True, write_function=callback)
+buffer = pypixelbuf.PixelBuf(20, bytearray(20 * 3), pypixelbuf.RGB, 1.0, auto_write=True,
+                             write_function=callback)
 
 buffer[0] = (1, 2, 3)
 
 print(callback_called)
-
