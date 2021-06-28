3119ee9469a0b396f6af68e37cffc06a5f2a7b76
Jeff Epler <jepler@gmail.com>
2021-06-28 14:16:36 -0500

    colorwheel: switch to returning integers
    .. rather than tuples.  This is faster and aligns with _pypixelbuf.colorwheel.

--- adafruit_pypixelbuf.py
+++ adafruit_pypixelbuf.py
@@ -350,14 +350,22 @@ def colorwheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
     if pos < 0 or pos > 255:
-        return 0, 0, 0
-    if pos < 85:
-        return 255 - pos * 3, pos * 3, 0
-    if pos < 170:
+        r = g = b = 0
+    elif pos < 85:
+        r = 255 - pos * 3
+        g = pos * 3
+        b = 0
+    elif pos < 170:
         pos -= 85
-        return 0, 255 - pos * 3, pos * 3
-    pos -= 170
-    return pos * 3, 0, 255 - pos * 3
+        r = 0
+        g = 255 - pos * 3
+        b = pos * 3
+    else:
+        pos -= 170
+        r = pos * 3
+        g = 0
+        b = 255 - pos * 3
+    return (r << 16) | (g << 8) | b
 
 # If _pypixelbuf is available, replace the content of this module
 # with the faster implementation from the core
